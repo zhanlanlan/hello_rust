264e6f22325997babd67bcb2da0780b5364264c3
liuyuan <liuy@yunphant.com>
2020-04-08 17:52:44 +0800

    another bug

--- src/lib/sorts.rs
+++ src/lib/sorts.rs
@@ -24,32 +24,33 @@ fn insert_sort(mut nums: Vec<i64>) -> Vec<i64> {
     nums
 }
 
+// 为什么不允许加减法越界只允许数组下标越界
 fn quick_sort(mut nums: Vec<i64>) -> Vec<i64> {
+    fn sort(mut nums: Vec<i64>, l: usize, r: usize) -> Vec<i64> {
+        if l < r {
+            let (mut xx, q) = partition(nums, l, r);
+            xx = sort(xx, l, q - 1);
+            xx = sort(xx, q + 1, r);
+            nums = xx
+        }
+        nums
+    };
+
+    fn partition(mut nums: Vec<i64>, l: usize, r: usize) -> (Vec<i64>, usize) {
+        let x = nums[r];
+        let mut i = l.wrapping_sub(1);
+        for j in l..r {
+            if nums[j] <= x {
+                i = i.wrapping_add(1);
+                nums.swap(i, j);
+            }
+        }
+        nums.swap(i + 1, r);
+        (nums, i + 1)
+    };
+
     let len = nums.len();
     sort(nums, 0, len - 1)
-}
-
-fn sort(mut nums: Vec<i64>, l: usize, r: usize) -> Vec<i64> {
-    if l < r {
-        let (mut xx, q) = partition(nums, l, r);
-        xx = sort(xx, l, q - 1);
-        xx = sort(xx, q + 1, r);
-        nums = xx
-    }
-    nums
-}
-
-fn partition(mut nums: Vec<i64>, l: usize, r: usize) -> (Vec<i64>, usize) {
-    let x = nums[r];
-    let mut i = l.checked_sub(1).unwrap_or(0); // fixme 这是一个bug
-    for j in l..r {
-        if nums[j] <= x {
-            i += 1;
-            nums.swap(i, j);
-        }
-    }
-    nums.swap(i + 1, r);
-    (nums, i + 1)
 }
 
 mod test {
@@ -79,7 +80,7 @@ mod test {
         println!(
             "{:?}",
             quick_sort(vec![
-                2, 32, 14, 21, 5, 43, 365, 66788, 437, 54, 7657, 65, 532, 43, 2, 99999, 0
+                2, 32, 14, 21, 5, 43, 365, 66788, 437, 54, 7657, 65, 532, 43, 2
             ])
         )
     }
